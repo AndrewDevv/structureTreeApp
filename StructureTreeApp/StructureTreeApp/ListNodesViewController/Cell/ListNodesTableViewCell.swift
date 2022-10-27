338edbf408d8d55b12df0da398583b75c495720c
AndreyAntonov <andru2517@bk.ru>
2022-10-27 15:35:40 +0400

    create project

---
+++ StructureTreeApp/StructureTreeApp/ListNodesViewController/Cell/ListNodesTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ListNodesTableViewCell.swift
+//  StructureTreeApp
+//
+//  Created by Андрей Антонов on 27.10.2022.
+//  Copyright © 2022 Андрей Антонов. All rights reserved.
+//
+
+import UIKit
+
+class ListNodesTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
---
+++ StructureTreeApp/StructureTreeApp/ListNodesViewController/ListNodesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ListNodesViewController.swift
+//  StructureTreeApp
+//
+//  Created by Андрей Антонов on 27.10.2022.
+//  Copyright © 2022 Андрей Антонов. All rights reserved.
+//
+
+import UIKit
+
+class ListNodesViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
