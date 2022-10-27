25761e463b7f75b9b5498925cf6b067dd86e177e
AndreyAntonov <andru2517@bk.ru>
2022-10-27 16:56:00 +0400

    create UI for tableView and tableViewCell

--- StructureTreeApp/StructureTreeApp/Root/AppDelegate.swift
+++ StructureTreeApp/StructureTreeApp/Root/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ListNodesViewController()
+        let listNodeViewController = ListNodesViewController()
+        let navController = UINavigationController()
+        
+        navController.viewControllers = [listNodeViewController]
+        window.rootViewController = navController
         window.makeKeyAndVisible()
         self.window = window
         
